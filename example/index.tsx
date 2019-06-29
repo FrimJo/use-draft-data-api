7d4bda7b78dfedb4e660cbf68b0c979569dd616a
Fredrik Johansson <fredrik.johansson@thnx.se>
2019-06-30 01:13:24 +0200

    Add example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,47 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import useDraftDataApi from '../src';
+
+type Payload = {
+  readonly data: Readonly<{
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+  }>;
+};
 
 const App = () => {
+  const [
+    {
+      data: { data },
+      isLoading,
+    },
+    doFetch,
+  ] = useDraftDataApi<Payload>(
+    'https://reqres.in/api/users/2',
+    {
+      data: { id: 0, email: '', first_name: '', last_name: '', avatar: '' },
+    },
+    (prevState, action) => action.draft
+  );
+
+  const { id, email, first_name, last_name, avatar } = data;
+
   return (
     <div>
-      <Thing />
+      {isLoading ? (
+        '...Loading'
+      ) : (
+        <>
+          <p>id: {id}</p>
+          <p>email: {email}</p>
+          <p>name: {`${first_name} ${last_name}`}</p>
+          <img src={avatar} />
+        </>
+      )}
     </div>
   );
 };
